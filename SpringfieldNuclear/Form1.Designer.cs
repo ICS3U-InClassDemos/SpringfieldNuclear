acf5bf733dfb7715e8936b4aebdeee6a2e766243
Tony Theodoropoulos <theodot@outlook.com>
2021-04-23 09:34:43 -0400

    Add project files.

---
+++ SpringfieldNuclear/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿
+namespace SpringfieldNuclear
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.titleLabel = new System.Windows.Forms.Label();
+            this.reactor1Label = new System.Windows.Forms.Label();
+            this.reactor2Label = new System.Windows.Forms.Label();
+            this.reactor1StateLabel = new System.Windows.Forms.Label();
+            this.reactor2StateLabel = new System.Windows.Forms.Label();
+            this.outputLabel = new System.Windows.Forms.Label();
+            this.startButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // titleLabel
+            // 
+            this.titleLabel.Font = new System.Drawing.Font("Gill Sans MT", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.titleLabel.Location = new System.Drawing.Point(23, 20);
+            this.titleLabel.Name = "titleLabel";
+            this.titleLabel.Size = new System.Drawing.Size(403, 57);
+            this.titleLabel.TabIndex = 0;
+            this.titleLabel.Text = "Nuclear Reactor Control";
+            this.titleLabel.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // reactor1Label
+            // 
+            this.reactor1Label.Font = new System.Drawing.Font("Palatino Linotype", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.reactor1Label.Location = new System.Drawing.Point(73, 82);
+            this.reactor1Label.Name = "reactor1Label";
+            this.reactor1Label.Size = new System.Drawing.Size(127, 29);
+            this.reactor1Label.TabIndex = 1;
+            this.reactor1Label.Text = "Reactor 1";
+            this.reactor1Label.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // reactor2Label
+            // 
+            this.reactor2Label.Font = new System.Drawing.Font("Palatino Linotype", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.reactor2Label.Location = new System.Drawing.Point(241, 82);
+            this.reactor2Label.Name = "reactor2Label";
+            this.reactor2Label.Size = new System.Drawing.Size(127, 29);
+            this.reactor2Label.TabIndex = 2;
+            this.reactor2Label.Text = "Reactor 2";
+            this.reactor2Label.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // reactor1StateLabel
+            // 
+            this.reactor1StateLabel.BackColor = System.Drawing.Color.Green;
+            this.reactor1StateLabel.Location = new System.Drawing.Point(75, 129);
+            this.reactor1StateLabel.Name = "reactor1StateLabel";
+            this.reactor1StateLabel.Size = new System.Drawing.Size(125, 125);
+            this.reactor1StateLabel.TabIndex = 3;
+            // 
+            // reactor2StateLabel
+            // 
+            this.reactor2StateLabel.BackColor = System.Drawing.Color.Green;
+            this.reactor2StateLabel.Location = new System.Drawing.Point(243, 129);
+            this.reactor2StateLabel.Name = "reactor2StateLabel";
+            this.reactor2StateLabel.Size = new System.Drawing.Size(125, 125);
+            this.reactor2StateLabel.TabIndex = 4;
+            // 
+            // outputLabel
+            // 
+            this.outputLabel.BackColor = System.Drawing.Color.White;
+            this.outputLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.outputLabel.Location = new System.Drawing.Point(12, 335);
+            this.outputLabel.Name = "outputLabel";
+            this.outputLabel.Size = new System.Drawing.Size(423, 50);
+            this.outputLabel.TabIndex = 5;
+            this.outputLabel.Text = "Reactors Stable";
+            this.outputLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // startButton
+            // 
+            this.startButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Fuchsia;
+            this.startButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.startButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.startButton.Location = new System.Drawing.Point(139, 280);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(167, 37);
+            this.startButton.TabIndex = 6;
+            this.startButton.Text = "Start Reactors";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Khaki;
+            this.ClientSize = new System.Drawing.Size(447, 414);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.outputLabel);
+            this.Controls.Add(this.reactor2StateLabel);
+            this.Controls.Add(this.reactor1StateLabel);
+            this.Controls.Add(this.reactor2Label);
+            this.Controls.Add(this.reactor1Label);
+            this.Controls.Add(this.titleLabel);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Nuclear Status";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label titleLabel;
+        private System.Windows.Forms.Label reactor1Label;
+        private System.Windows.Forms.Label reactor2Label;
+        private System.Windows.Forms.Label reactor1StateLabel;
+        private System.Windows.Forms.Label reactor2StateLabel;
+        private System.Windows.Forms.Label outputLabel;
+        private System.Windows.Forms.Button startButton;
+    }
+}
+
---
+++ SpringfieldNuclear/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Media; // allows us to use a sound player
+
+namespace SpringfieldNuclear
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            //change background colour of form
+            this.BackColor = Color.DodgerBlue;
+
+            //create sound player and play sound
+            SoundPlayer alertPlayer = new SoundPlayer(Properties.Resources.alert);
+            alertPlayer.Play();
+
+            //change the colour of the state labels
+            reactor1StateLabel.BackColor = Color.Red;
+            reactor2StateLabel.BackColor = Color.Red;
+
+            //change the message in output label
+            outputLabel.Text = "Meltdown Happening";
+            startButton.Visible = false;
+
+            //show changes and then pause
+            Refresh();
+            Thread.Sleep(1000);
+
+            //change the colour of state lables to white
+            reactor1StateLabel.BackColor = Color.White;
+            reactor2StateLabel.BackColor = Color.White;
+
+            //show changes and then pause
+            Refresh();
+            Thread.Sleep(1000);
+
+            //change the colour of the state labels
+            reactor1StateLabel.BackColor = Color.Red;
+            reactor2StateLabel.BackColor = Color.Red;
+        }
+    }
+}
